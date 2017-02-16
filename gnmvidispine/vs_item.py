0a13d5df7fb4f64acf3c19fba7169bedf5c644af
Andy Gallagher <andy.gallagher@theguardian.com>
2017-02-16 11:04:55 +0000

    allowing non-specific mode in vsmetadatabuilder

--- gnmvidispine/vs_item.py
+++ gnmvidispine/vs_item.py
@@ -897,11 +897,14 @@ class VSMetadataBuilder(VSApi):
         :param subgroupmode:
         :return:
         """
+        params = {}
+        if subgroupmode is not None:
+            params={'mode': subgroupmode}
         for key,value in meta.items():
             #print key
             #pprint(value)
             if isinstance(value,dict):
-                subgroupnode = ET.SubElement(parentNode,"group",{'mode': subgroupmode})
+                subgroupnode = ET.SubElement(parentNode,"group",params)
                 subgroupname = ET.SubElement(subgroupnode,"name")
                 subgroupname.text = key
                 self._groupContent(subgroupnode,value)
@@ -935,7 +938,11 @@ class VSMetadataBuilder(VSApi):
         if subgroubmode is None:
             subgroupmode = mode
 
-        groupnode = ET.SubElement(self.tsNode,"group",{'mode': mode})
+        params = {}
+        if mode is not None:
+            params={'mode': mode}
+            
+        groupnode = ET.SubElement(self.tsNode,"group",params)
         groupnameNode = ET.SubElement(groupnode,"name")
         groupnameNode.text = groupname
 
