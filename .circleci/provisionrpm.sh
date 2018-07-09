a9b272b5832b8ad59a0c5d53faee4124dce5f631
Andy Gallagher <andy.gallagher@theguardian.com>
2018-07-09 15:03:35 +0100

    new image build

--- .circleci/provisionrpm.sh
+++ .circleci/provisionrpm.sh
@@ -5,7 +5,7 @@ useradd circleci
 yum -y clean all
 yum -y install epel-release
 #other packages that were present on ubuntu: libsasl2-devel libldap2-devel libssl-devel
-yum -y install git python python2-pip python-virtualenv doxygen
+yum -y install git python python2-pip python-virtualenv doxygen rpm-build
 
 mkdir -p /opt/cantemo/python/
 chown -R circleci /opt/cantemo/python/
