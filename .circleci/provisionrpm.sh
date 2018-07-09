affffe01b4e37fb7511461e5fddfeb60f8bd8909
Andy Gallagher <andy.gallagher@theguardian.com>
2018-07-09 14:58:13 +0100

    circleci2 initial test

---
+++ .circleci/provisionrpm.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash -e
+
+useradd circleci
+
+yum -y clean all
+yum -y install epel-release
+#other packages that were present on ubuntu: libsasl2-devel libldap2-devel libssl-devel
+yum -y install git python python2-pip python-virtualenv doxygen
+
+mkdir -p /opt/cantemo/python/
+chown -R circleci /opt/cantemo/python/
+yum -y remove epel-release
+
+yum -y clean all
+rm -rf /var/cache/yum
