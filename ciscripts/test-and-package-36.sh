47433b3553041cd3ae131bbc9e689e70f680989e
Andy Gallagher <andy.gallagher@theguardian.com>
2019-12-16 11:55:29 +0000

    move to hardlinkable location for rpm build, complete TC build

--- ciscripts/test-and-package-36.sh
+++ ciscripts/test-and-package-36.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash -e
+#!/bin/bash -e
 
 ### this script expects to be run from a docker image andyg42/centos-python36-build
 ### it expects the environment variables ${UPLOAD_BUCKET} and ${CIRCLE_BUILD_NUM} to be set
@@ -9,8 +9,24 @@ pip install -r requirements.txt
 pip install boto
 
 declare -x PYTHONPATH=${HOME}/virtualenvs/gnmvidispine/lib/python3.6/site-packages
-nosetests-3.6 --with-xunit tests/
+nosetests-3.6 tests/
 
-./setup_py3.py sdist
-./setup_py3.py buildrpm
-./setup_py3.py awsupload --bucket ${UPLOAD_BUCKET} --path gnmvidispine --region eu-west-1  --acl public-read
+
+## In Teamcity, the source directory is a bind-mount over to the main server storage which appears not to support
+## hardlinking which is required for RPM build. So we copy into the container's filesystem here.
+SOURCE_DIR=$PWD
+echo Original source directory is ${SOURCE_DIR}
+ENDPART=`basename ${SOURCE_DIR}`
+mkdir -p /usr/src/${ENDPART}
+cp -a ${SOURCE_DIR}/ /usr/src || /bin/true
+
+cd /usr/src/${ENDPART}
+
+python3 ./setup_py3.py sdist
+python3 ./setup_py3.py buildrpm
+
+if [ ! -d "${SOURCE_DIR}/dist" ]; then
+    mkdir -p ${SOURCE_DIR}/dist
+fi
+
+cp -rv dist/* ${SOURCE_DIR}/dist
